fe31828c3b24a72dba710350560a177ec8a86dc0
山脇舜介 <shunsukeyyy@gmail.com>
2014-06-12 23:17:09 +0900

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,11 +4,12 @@ describe "User Pages" do
 
 	subject { page }
 
-  describe "singleup page" do
-    before { visit signup_path }
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
   end
 
   describe "signup page" do
@@ -19,7 +20,6 @@ describe "User Pages" do
   end
 
   describe "signup" do
-
     before { visit signup_path }
 
     let(:submit) { "Create my account" }
@@ -40,6 +40,15 @@ describe "User Pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User,:count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text:'Welcome') }
       end
     end
   end
